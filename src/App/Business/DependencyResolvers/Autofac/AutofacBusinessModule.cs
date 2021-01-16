8675fb9bc20b41cf0f3526bd75a0b80c52cd594f
yasartaymaz.home <yasartaymaz@windowslive.com>
2021-01-16 20:44:29 +0300

    AutofacBusinessModule eklendi. account icin tanımlamalar yapıldı.

---
+++ src/App/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Operations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            #region business layer
+
+            builder.RegisterType<AccountManager>().As<IAccountService>();
+
+            #endregion
+
+            #region dataAccess layer
+
+            builder.RegisterType<EfAccountDA>().As<IAccountDA>();
+
+            #endregion
+
+            base.Load(builder);
+        }
+    }
+}
